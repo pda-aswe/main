277e83837cfa370feea7bfcbcf66828ffa5af0b6
Fabian Kuffer <fabian.kuffer@kuffer.info>
2023-03-14 21:12:00 +0100

    tts and stt added

--- src/main.py
+++ src/main.py
@@ -9,7 +9,13 @@ from datetime import datetime, timedelta
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import paho.mqtt.client as mqtt
-import tkinter
+from tkinter import *
+import customtkinter
+import sounddevice as sd
+from vosk import Model, KaldiRecognizer
+import queue
+import pyttsx3
+import platform
 
 containerNames = ["bard-broker","bard-casepreis", "bard-casesport", "bard-casenews", "bard-casewelcome", "bard-servicewetter", "bard-serviceverkehrsinfos", "bard-servicekalender", "bard-servicenews", "bard-servicelocation", "bard-servicesport", "bard-servicepreis", "bard-servicemail"]
 
@@ -18,6 +24,11 @@ preferences = {}
 preferencePath = os.path.join(os.getcwd(), 'preferences.json')
 
 speakThread = None
+STTRecognizer = None
+TTSEngine = None
+
+#queue for audio chunks
+audioQueue = queue.Queue()
 
 def readPreferences(path):
     try:
@@ -80,7 +91,7 @@ def jsonDifference(oldData,newData):
                     
 
 def onConnectMQTT(client,userdata,flags, rc):
-    client.subscribe([("req/pref/#",0),])
+    client.subscribe([("req/pref/#",0),("tts",0)])
 
 def preferenceMQTTCallback(client, userdata, msg):
     if msg.topic == "req/pref/all":
@@ -103,6 +114,14 @@ def preferenceMQTTCallback(client, userdata, msg):
         if returnData is not None:
             sendPreferenceStructure(client,"/".join(keys[1:]),returnData)
 
+def sendSTTMessage(message):
+    client.publish("stt",str(message))
+    #remove later
+    client.publish("tts",str(message))
+
+def sttMQTTCallback(client, userdata, msg):
+    TTSEngine.say(str(msg.payload.decode("utf-8")))
+    TTSEngine.runAndWait()
 
 #Diese Funktion wird aufgerufen, wenn es für ein Topic kein spezielles Callback gibt
 def onMessageMQTT(client, userdata, msg):
@@ -110,19 +129,30 @@ def onMessageMQTT(client, userdata, msg):
 
 def speakRecognition():
     t = threading.current_thread()
-    while getattr(t, "run", True):
-        print ("Recording")
-        time.sleep(0.5)
+    with sd.RawInputStream(dtype='int16',channels=1,callback=recordCallback):
+        while getattr(t, "run", True):
+           pass
     return
 
 def speakPress(event=None):
     global speakThread
+    global STTRecognizer
     if event and int(event.type) == 4:
         speakThread = threading.Thread(target=speakRecognition)
         speakThread.start()
     elif event and int(event.type) == 5:
         speakThread.run = False
         speakThread.join()
+        while not audioQueue.empty():
+            data = audioQueue.get()   
+            STTRecognizer.AcceptWaveform(data)
+        resultDict = json.loads(STTRecognizer.FinalResult())
+        if not resultDict.get("text", "") == "":
+            print(resultDict["text"])
+            sendSTTMessage(resultDict["text"])
+
+def recordCallback(indata, frames, time, status):
+    audioQueue.put(bytes(indata))
 
 if __name__ ==  "__main__":
     #connect to docker
@@ -130,7 +160,7 @@ if __name__ ==  "__main__":
     client = docker.from_env()
 
     #start all container
-    subprocess.run(["docker", "compose", "up", "-d"])
+    #subprocess.run(["docker", "compose", "up", "-d"])
 
     #setup mqtt client
     #aufbau der MQTT-Verbindung
@@ -138,6 +168,7 @@ if __name__ ==  "__main__":
     client.on_connect = onConnectMQTT
     client.on_message = onMessageMQTT
     client.message_callback_add("req/pref/#", preferenceMQTTCallback)
+    client.message_callback_add("tts", sttMQTTCallback)
     try:
         client.connect("localhost",1883,60)
     except:
@@ -151,25 +182,45 @@ if __name__ ==  "__main__":
     observer.schedule(event_handler,  path=preferencePath,  recursive=False)
     observer.start()
 
+    #setup microphone
+    # get the samplerate - this is needed by the Kaldi recognizer
+    device_info = sd.query_devices(sd.default.device[0], 'input')
+    samplerate = int(device_info['default_samplerate'])
+    # display the default input device
+    print("Microphone: {}".format(device_info['name']))
+
+    #setup STT
+    model = Model(lang="de")
+    STTRecognizer = KaldiRecognizer(model, samplerate)
+    STTRecognizer.SetWords(True)
+
+    #setup TTS
+    TTSEngine = pyttsx3.init()
+    if platform.system() == "Windows":
+        TTSEngine.setProperty('voice', "LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_DE-DE_HEDDA_11.0")
+        TTSEngine.setProperty('rate', TTSEngine.getProperty('rate')-50)
+
     #send preferences from preferences.json
     preferences = readPreferences(preferencePath)
     sendPreferenceStructure(client,"pref",preferences)
 
     #create main window
-    mainWindow = tkinter.Tk()
+    customtkinter.set_appearance_mode("dark")
+    customtkinter.set_default_color_theme("green")
+    mainWindow = customtkinter.CTk()
+    mainWindow.geometry("400x240")
     mainWindow.title('BARD')
-    button = tkinter.Button(mainWindow, text='Speak', width=25)
+    button = customtkinter.CTkButton(master=mainWindow, text='Speak')
     button.bind('<ButtonPress-1>',speakPress)
     button.bind('<ButtonRelease-1>',speakPress)
-    button.pack()
+    button.place(relx=0.5,rely=0.4,anchor=CENTER)
     mainWindow.mainloop()
 
     #stop all container
-    subprocess.run(["docker", "compose", "stop"])
+    #subprocess.run(["docker", "compose", "stop"])
 
     #stop file watchdog
     observer.stop()
-    observer.join()
 
     #stop mqtt
     client.loop_stop()
